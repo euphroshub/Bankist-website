78a9dad94180ef6f12059675eb4e03696a94e964
euphroshub <euphros@protonmail.com>
2021-11-23 09:53:59 -0500

    smooth scrolling to section 1

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 };
 
 
@@ -25,10 +25,32 @@ btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
 });
 
 // End modal window
 //////////////////////////////////////////
+
+
+
+// Smooth scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function(e) {
+    //const s1coords = section1.getBoundingClientRect();
+
+    // Scrolling (oldschool)
+/*     window.scrollTo({
+        left: s1coords.left + window.pageXOffset,
+        top: s1coords.top + window.pageYOffset,
+        behavior: 'smooth',
+    }); */
+
+    // New modern way (modern browser only)
+    section1.scrollIntoView({behavior: 'smooth'});
+
+});
