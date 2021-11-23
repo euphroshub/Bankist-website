ce0e34fc2fa7442fafc6653563dc372f1d7c9df0
euphroshub <euphros@protonmail.com>
2021-11-23 10:59:02 -0500

    nav item smooth scrolling

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 'use strict';
 
-// Modal window
-
+// Elements
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+// Modal window
 
 const openModal = function (e) {
     e.preventDefault();
@@ -17,7 +20,6 @@ const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 };
-
 
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
@@ -34,11 +36,7 @@ document.addEventListener('keydown', function (e) {
 //////////////////////////////////////////
 
 
-
 // Smooth scrolling
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function(e) {
     //const s1coords = section1.getBoundingClientRect();
@@ -54,3 +52,35 @@ btnScrollTo.addEventListener('click', function(e) {
     section1.scrollIntoView({behavior: 'smooth'});
 
 });
+
+// End smooth scrolling
+//////////////////////////////////////////
+
+
+// Page navigation (standard (not useful when 1000s of elements))
+
+/* document.querySelectorAll('.nav__link').forEach(function(el) {
+    el.addEventListener('click', function(e) {
+        e.preventDefault();
+        const id = this.getAttribute('href');
+        document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+    });
+}); */
+
+// (EVENT DELEGATION)
+// 1. add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // Matching strategy
+    if (e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+    }
+});
+
+
+// End page navigation
+//////////////////////////////////////////
