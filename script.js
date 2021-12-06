628ecd0503f4a3428e5588e9c87c7f0128056150
euphroshub <euphros@protonmail.com>
2021-12-06 10:06:38 -0500

    nav links fade animation

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
 
 // Modal window
 
@@ -86,11 +91,6 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
 
 
 // Tabbed component
-// Getting elements
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 // Event listener function
 tabsContainer.addEventListener('click', function(e) {
     const clicked = e.target.closest('.operations__tab');
@@ -109,3 +109,24 @@ tabsContainer.addEventListener('click', function(e) {
     document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 
 });
+
+
+// Menu fade animation
+const handleHover = function(e) {
+    if (e.target.classList.contains('nav__link')) {
+        const link = e.target;
+        const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+        const logo = link.closest('.nav').querySelector('img');
+
+        siblings.forEach(el => {
+            if (el !== link) el.style.opacity = this;
+        });
+
+        logo.style.opacity = this;
+    }
+}
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
+
