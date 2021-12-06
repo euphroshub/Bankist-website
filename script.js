160476bf9295c09df057c22e4bbf80d732450b1b
euphroshub <euphros@protonmail.com>
2021-12-06 17:36:20 -0500

    slider component

--- script.js
+++ script.js
@@ -201,3 +201,87 @@ const imgObserver = new IntersectionObserver
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+
+// Slider component
+const slider = function() {
+    const slides = document.querySelectorAll('.slide');
+    const btnLeft = document.querySelector('.slider__btn--left');
+    const btnRight = document.querySelector('.slider__btn--right');
+    const dotContainer = document.querySelector('.dots');
+    let curSlide = 0;
+    const maxSlide = slides.length;
+    const slider = document.querySelector('.slider');
+    
+    // Creating the dots under the slider
+    const createDots = function() {
+        slides.forEach(function(_, i) {
+            dotContainer.insertAdjacentHTML('beforeend',
+            `<button class="dots__dot" data-slide="${i}"></button>`);
+        });
+    };
+    
+    const activateDot = function(slide) {
+        document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
+    
+        document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
+    }
+    
+    // Go to slide function
+    const goToSlide = function(slide) {
+        slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`);
+    }
+    
+    // Going to the next slide
+    const nextSlide = function() {
+        if (curSlide === maxSlide) {
+            curSlide = 0;
+        } else {
+            curSlide++;
+        }
+    
+        goToSlide(curSlide);
+        activateDot(curSlide);
+    }
+    
+    // Going to the previous slide
+    const prevSlide = function() {
+        if (curSlide === 0) {
+            curSlide = maxSlide -1;
+        } else {
+            curSlide--;
+        }
+    
+        goToSlide(curSlide);
+        activateDot(curSlide);
+    }
+    
+    const init = function() {
+        goToSlide(0);
+        createDots();
+        activateDot(0);
+    }
+
+    init();
+    
+    // Event handlers
+    btnRight.addEventListener('click', nextSlide);
+    btnLeft.addEventListener('click', prevSlide);
+    
+    // Navigating slides on key press (left and right keyboard arrow)
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'ArrowLeft') prevSlide();
+        if (e.key === 'ArrowRight') nextSlide();
+    });
+    
+    // Navigating the slides with the dots
+    dotContainer.addEventListener('click', function(e) {
+        if (e.target.classList.contains('dots__dot')) {
+            const {slide} = e.target.dataset;
+            goToSlide(slide);
+            activateDot(slide);
+        }
+    });
+}
+
+slider();
